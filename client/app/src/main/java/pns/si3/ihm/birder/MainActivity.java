aea3e03f8d38a1be5fec3387845f2d1c38bc93c5
Martin <martin.bouteiller@etu.unice.fr>
2020-05-08 10:43:39 +0200

    #8 Accéder à ActivitySignal depuis MainActivity (même si on ne gère pas encore la connexion, on remodifiera plus tard)

--- client/app/src/main/java/pns/si3/ihm/birder/MainActivity.java
+++ client/app/src/main/java/pns/si3/ihm/birder/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,13 +17,26 @@ import etudes.fr.demoosm.R;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSpinner();
 
+        button = (Button) findViewById(R.id.buttonMain);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSignalActivity();
+            }
+        });
+    }
 
+    public void goToSignalActivity() {
+        Intent intent = new Intent(this, SignalActivity.class);
+        startActivity(intent);
     }
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
@@ -48,11 +62,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void setSpinner(){
         final Spinner spinner = (Spinner) findViewById(R.id.spinner_main);
         spinner.setOnItemSelectedListener(this);
-        List<String> list = new ArrayList<String>();
-        list.add("Dernières signalisations");
+        List<String> list = new ArrayList<>();
+        list.add("Menu");
         list.add("Voir Carte");
         list.add("Se connecter");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
--- client/app/src/main/java/pns/si3/ihm/birder/SignalActivity.java
+++ client/app/src/main/java/pns/si3/ihm/birder/SignalActivity.java
@@ -1,4 +1,35 @@
 package pns.si3.ihm.birder;
 
-public class SignalActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import etudes.fr.demoosm.R;
+
+public class SignalActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signal);
+
+        button = (Button) findViewById(R.id.buttonSignalReturn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goBack();
+            }
+        });
+    }
+
+    public void goBack() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
