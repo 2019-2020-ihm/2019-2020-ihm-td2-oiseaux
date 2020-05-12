72a8191e544c1a7a5b3995c4cc9cb080133104de
Elise Chamberlin <elise.chamberlin@etu.unice.fr>
2020-05-12 20:56:35 +0200

    Ajout du partager des trophées obtenus sur les réseaux sociaux

--- client/app/src/main/java/pns/si3/ihm/birder/views/TrophyActivity.java
+++ client/app/src/main/java/pns/si3/ihm/birder/views/TrophyActivity.java
@@ -14,6 +14,7 @@ import pns.si3.ihm.birder.models.Trophy;
 
 public class TrophyActivity extends AppCompatActivity {
     Button buttonRetour;
+    Button shareTrophy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +36,19 @@ public class TrophyActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 goBack();
+            }
+        });
+
+        shareTrophy = findViewById(R.id.buttonTropheeShare);
+        shareTrophy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String shareBody = "J'ai obtenue le trophée \"" + trophy.getName() + "\" sur l'application Birder!";
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Le trophée");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Partager via"));
             }
         });
 
