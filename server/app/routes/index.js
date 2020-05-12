506bd5cf16a15149933f1b81f1bdf2eb9bcf1989
Joao Brilhante <joao.brilhante@outlook.com>
2020-05-12 03:20:57 +0200

    Remove unused server files (we use Firebase).

--- server/app/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const StatusRouter = require('./status');
-
-const router = new Router();
-
-// Status routes.
-router.use('/status', StatusRouter);
-
-module.exports = router;
--- server/app/utils/handlers/not-found-handler.js
+++
@@ -1,7 +0,0 @@
-const NotFoundError = require('../errors/not-found-error');
-
-const notFoundHandler = (req, res, next) => {
-  next(new NotFoundError());
-};
-
-module.exports = notFoundHandler;
